475763790a7dd819241edbfb707a3367888f32a3
rao7dssushi3 <guyan9959@163.com>
2025-11-12 22:00:32 +0800

    Create singleloss.py

---
+++ function/singleloss.py
@@ -0,0 +1,11 @@
+import torch
+
+def kd_loss_function(output, target_output,args):
+    output = output / args.temperature
+    output_log_softmax = torch.log_softmax(output, dim=1)
+    loss_kd = -torch.mean(torch.sum(output_log_softmax * target_output, dim=1))
+    return loss_kd
+
+def feature_loss_function(fea, target_fea):
+    loss = (fea - target_fea)**2 * ((fea > 0) | (target_fea > 0)).float()
+    return torch.abs(loss).sum()
